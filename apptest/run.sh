bf591ed8b447133a9425168c7a17826eeb8b67c4
yip-jm <64177370+yip-jm@users.noreply.github.com>
2025-11-06 23:33:35 +0800

    update

--- apptest/run.sh
+++ apptest/run.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1             # Request 1 GPU
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
 #SBATCH --partition=gpu_h100         # Use a general GPU partition like 'gpu' or 'gpu_shared'
 
